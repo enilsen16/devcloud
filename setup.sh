8e725171f9a6a254fef68ce3d6f309b57e7cf674
Erik Nilsen <enilsen16@live.com>
2018-04-24 20:06:36 +0200

    Setup phoenix dev enviroment

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# update ubuntu packages
+sudo apt-get update
+sudo apt-get full-upgrade
+sudo apt-get autoremove
+
+# Add postgres
+sudo apt-get install postgresql postgresql-contrib libpq-dev
+sudo -u postgres psql postgres
+\password postgres
+\q
+
+# Install Kerl/Erlang
+mkdir ~/.kerl
+curl -fsSLo ~/.kerl/kerl https://raw.githubusercontent.com/kerl/kerl/master/kerl
+chmod +x ~/.kerl/kerl
+
+~/.kerl/kerl update releases
+~/.kerl/kerl build git https://github.com/erlang/otp.git maint maint
+~/.kerl/kerl install maint ~/.kerl/maint
+~/.kerl/maint/activate
+~/.kerl cleanup all
+
+# Install elixir
+git clone https://github.com/elixir-lang/elixir.git ~/.elixir
+cd ~/.elixir
+make install
+cd ~/
+
+echo "Erlang and Elixir setup"
